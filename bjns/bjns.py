9fefa4e3204de477826d85b193ed0f93e0c53bae
35626 <ztyawc@gmail.com>
2023-09-03 19:18:19 +0800

    完全体

--- bjns/bjns.py
+++ bjns/bjns.py
@@ -1,6 +1,11 @@
 import re
+import time
+
 import requests
 import ding
+
+list={'cook':'','check-in_status':'','Lottery_results':'','fbcj':'','fb':''}
+
 
 def get_cookies():
     headers = {
@@ -33,14 +38,14 @@ def get_cookies():
         extracted_string = match.group(1)
         print(extracted_string)
         extracted_string=str(extracted_string)
-        return extracted_string
+        list['cook']=extracted_string
 
 def sign():
     import requests
 
     cookies = {
         'think_language': 'zh-CN',
-        'PHPSESSID': get_cookies(),
+        'PHPSESSID': list['cook'],
         'bjrcb_info': '04708D74644CD114E0E9CEE2B14222FE89EAC0CE1691F69B2838DD4CCFF6DD6133B302A6F20F3C771C1F801F1E9B019B1AFD4058B5EB26BF005C30040738F2365B458C8678483057D957ECBCF1F2484254FE30F5A2BD7FEF70CBA65CF954AEA7EB13332BC948B08EA646F5F5',
     }
 
@@ -70,14 +75,14 @@ def sign():
 
     response = requests.post('https://bjrcb.mocentre.cn/Wap/PolyPageSign/signIn', cookies=cookies, headers=headers,
                              data=data)
-    return str(response.json()['msg'])
+    list['check-in_status']= str(response.json()['msg'])
 
 
 
 def lottery():
     cookies = {
         'think_language': 'zh-CN',
-        'PHPSESSID': 'rvvrngvplar4kn2tm6lved24o3',
+        'PHPSESSID': list['cook'],
         'bjrcb_info': '04708D74644CD114E0E9CEE2B14222FE89EAC0CE1691F69B2838DD4CCFF6DD6133B302A6F20F3C771C1F801F1E9B019B1AFD4058B5EB26BF005C30040738F2365B458C8678483057D957ECBCF1F2484254FE30F5A2BD7FEF70CBA65CF954AEA7EB13332BC948B08EA646F5F5',
         'public_ticket': 'ee7b2c7e4747425bf937f599b586fb19',
         'jingqd0518_user': '%7B%22id%22%3A%22aC6Qg0Z3Wo7NcPZXV6IHYA%3D%3D%22%2C%22user_id%22%3A%220RILaEiBgVKSjJOCLmt3xw%3D%3D%22%2C%22user_name%22%3A%22%22%2C%22openid%22%3A%22%22%2C%22public_userid%22%3A%220%22%7D',
@@ -113,9 +118,95 @@ def lottery():
     }
 
     response = requests.post('https://bjrcb.mocentre.cn/index.php', params=params, cookies=cookies, headers=headers)
-    return str(response.json()['msg'])
+    list['Lottery_results']=str(response.json()['msg'])
 
-strs=sign()
-strl=lottery()
 
-ding.send_md(title="测试", text="# 北京农商银行 \n ## •  签到："+strs+"\n## •  抽奖："+strl+"", isAtAll=True)
+def cj():
+
+    cookies = {
+        'think_language': 'zh-CN',
+        'PHPSESSID': list['cook'],
+        'fenghuang1_num': '0',
+        'fenghuang1_user': '%7B%22id%22%3A%22aC6Qg0Z3Wo7NcPZXV6IHYA%3D%3D%22%2C%22user_id%22%3A%220RILaEiBgVKSjJOCLmt3xw%3D%3D%22%2C%22user_name%22%3A%22%22%2C%22openid%22%3A%22%22%2C%22public_userid%22%3A%220%22%7D',
+    }
+
+    headers = {
+        'Host': 'bjrcb.mocentre.cn',
+        'Connection': 'keep-alive',
+        # 'Content-Length': '0',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 13; M2012K11C Build/TKQ1.220829.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/104.0.5112.97 Mobile Safari/537.36 App/BJRCB',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Origin': 'https://bjrcb.mocentre.cn',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Dest': 'empty',
+        'Referer': 'https://bjrcb.mocentre.cn/Wap/LotteryDrawCommon/index/alone/1/token/mocentreabc/lotteryKey/fenghuang1.html',
+        # 'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+        # 'Cookie': 'think_language=zh-CN; PHPSESSID=apjlsgsr1fgb8hiqsikiia79ee; bjrcb_info=04744543D49DA3FF8A42E607BA9BFF7D70221CA88E4EF42FD95172EEFCBAAD9CD618F3136A94F503A626CA9ABDE3024E51E6B0D9DE7185B80AAFD720473B91CA99D59730FC340B7E7C9AA744625309CFAE787DB1BC0F12D350B268FD1E896B419214C03DE62B7FCFE5F3757B; public_ticket=7bf7e597b9a4fe1bee795d0283cbb2d6; fenghuang1_num=0; fenghuang1_user=%7B%22id%22%3A%22aC6Qg0Z3Wo7NcPZXV6IHYA%3D%3D%22%2C%22user_id%22%3A%220RILaEiBgVKSjJOCLmt3xw%3D%3D%22%2C%22user_name%22%3A%22%22%2C%22openid%22%3A%22%22%2C%22public_userid%22%3A%220%22%7D',
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+
+    params = {
+        'm': 'Wap',
+        'c': 'LotteryDrawCommon',
+        'a': 'decIntegralLottery',
+        'alone': '1',
+        'token': 'mocentreabc',
+        'lotteryKey': 'fenghuang1',
+    }
+
+    response = requests.post('https://bjrcb.mocentre.cn/index.php', params=params, cookies=cookies, headers=headers)
+    list['fbcj']=str(response.json()['msg'])
+def cx():
+    import requests
+
+    cookies = {
+        'think_language': 'zh-CN',
+        'PHPSESSID': list['cook'],
+        'bjrcb_info': '04744543D49DA3FF8A42E607BA9BFF7D70221CA88E4EF42FD95172EEFCBAAD9CD618F3136A94F503A626CA9ABDE3024E51E6B0D9DE7185B80AAFD720473B91CA99D59730FC340B7E7C9AA744625309CFAE787DB1BC0F12D350B268FD1E896B419214C03DE62B7FCFE5F3757B',
+        'public_ticket': '7bf7e597b9a4fe1bee795d0283cbb2d6',
+        'fenghuang1_user': '%7B%22id%22%3A%22aC6Qg0Z3Wo7NcPZXV6IHYA%3D%3D%22%2C%22user_id%22%3A%220RILaEiBgVKSjJOCLmt3xw%3D%3D%22%2C%22user_name%22%3A%22%22%2C%22openid%22%3A%22%22%2C%22public_userid%22%3A%220%22%7D',
+        'fenghuang1_num': '1',
+        'fenghuang1_num': '1',
+    }
+
+    headers = {
+        'Host': 'bjrcb.mocentre.cn',
+        'Connection': 'keep-alive',
+        # 'Content-Length': '0',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 13; M2012K11C Build/TKQ1.220829.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/104.0.5112.97 Mobile Safari/537.36 App/BJRCB',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Origin': 'https://bjrcb.mocentre.cn',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Dest': 'empty',
+        'Referer': 'https://bjrcb.mocentre.cn/Wap/LotteryDrawCommon/index/alone/1/token/mocentreabc/lotteryKey/fenghuang1.html',
+        # 'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+        # 'Cookie': 'think_language=zh-CN; PHPSESSID=apjlsgsr1fgb8hiqsikiia79ee; bjrcb_info=04744543D49DA3FF8A42E607BA9BFF7D70221CA88E4EF42FD95172EEFCBAAD9CD618F3136A94F503A626CA9ABDE3024E51E6B0D9DE7185B80AAFD720473B91CA99D59730FC340B7E7C9AA744625309CFAE787DB1BC0F12D350B268FD1E896B419214C03DE62B7FCFE5F3757B; public_ticket=7bf7e597b9a4fe1bee795d0283cbb2d6; fenghuang1_user=%7B%22id%22%3A%22aC6Qg0Z3Wo7NcPZXV6IHYA%3D%3D%22%2C%22user_id%22%3A%220RILaEiBgVKSjJOCLmt3xw%3D%3D%22%2C%22user_name%22%3A%22%22%2C%22openid%22%3A%22%22%2C%22public_userid%22%3A%220%22%7D; fenghuang1_num=1; fenghuang1_num=1',
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+
+    params = {
+        'm': 'Wap',
+        'c': 'LotteryDrawCommon',
+        'a': 'lotteryResult',
+        'alone': '1',
+        'token': 'mocentreabc',
+        'lotteryKey': 'fenghuang1',
+    }
+
+    response = requests.post('https://bjrcb.mocentre.cn/index.php', params=params, cookies=cookies, headers=headers)
+    list['fb']=str(response.json()['msg'])
+get_cookies()
+
+sign()
+lottery()
+cj()
+time.sleep(2)
+cx()
+
+ding.send_md(title="测试", text="# 北京农商银行 \n ## •  签到："+list['check-in_status']+"\n## •  抽奖："+list['Lottery_results']+"\n## •  翻倍抽奖状态："+list['fbcj']+"\n## •  翻倍抽奖奖品："+list['fb']+"\n", isAtAll=True)
--- bjns/dsf.py
+++ bjns/dsf.py
@@ -1,31 +1,45 @@
+import re
 import requests
-import bjns
-cookies = {
-    'think_language': 'zh-CN',
-    'PHPSESSID': bjns.get_cookies(),
-    'bjrcb_info': '04B4ED68F430C5C0EB0FC747913BA21B06B395FA476B3B211F1737507EC1C65DF6BCBCF7F5569CA6B17EDF9312D5DE8CC6AC02EBC970958D6802BA81D6BAA1D741168D3DDAA089CF6B9075693692AE1392FCC685B361F1AC8371DDED2DF54BB342025A1B095073292789943E',
-}
 
-headers = {
-    'Host': 'bjrcb.mocentre.cn',
-    'Connection': 'keep-alive',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Linux; Android 13; M2012K11C Build/TKQ1.220829.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/104.0.5112.97 Mobile Safari/537.36 App/BJRCB',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'X-Requested-With': 'cn.com.bjns.mbank',
-    'Sec-Fetch-Site': 'same-origin',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-User': '?1',
-    'Sec-Fetch-Dest': 'document',
-    'Referer': 'https://bjrcb.mocentre.cn/Wap/PolyPage/index?keymark=HdzqName&login_flag=1&Cid=04B4ED68F430C5C0EB0FC747913BA21B06B395FA476B3B211F1737507EC1C65DF6BCBCF7F5569CA6B17EDF9312D5DE8CC6AC02EBC970958D6802BA81D6BAA1D741168D3DDAA089CF6B9075693692AE1392FCC685B361F1AC8371DDED2DF54BB342025A1B095073292789943E',
-    # 'Accept-Encoding': 'gzip, deflate',
-    'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-    # 'Cookie': 'think_language=zh-CN; PHPSESSID=ak7q655p8c24ni7auceik34h7b; bjrcb_info=04B4ED68F430C5C0EB0FC747913BA21B06B395FA476B3B211F1737507EC1C65DF6BCBCF7F5569CA6B17EDF9312D5DE8CC6AC02EBC970958D6802BA81D6BAA1D741168D3DDAA089CF6B9075693692AE1392FCC685B361F1AC8371DDED2DF54BB342025A1B095073292789943E',
-}
+def fb():
+    cookies = {
+        'think_language': 'zh-CN',
+        'PHPSESSID': 'gjcsrrejd54fkmr5fhdr24orqa',
+        'bjrcb_info': '04B4ED68F430C5C0EB0FC747913BA21B06B395FA476B3B211F1737507EC1C65DF6BCBCF7F5569CA6B17EDF9312D5DE8CC6AC02EBC970958D6802BA81D6BAA1D741168D3DDAA089CF6B9075693692AE1392FCC685B361F1AC8371DDED2DF54BB342025A1B095073292789943E',
+    }
 
-response = requests.get(
-    'https://bjrcb.mocentre.cn/Wap/LotteryDrawCommon/index/alone/1/token/mocentreabc/lotteryKey/fenghuang1.html',
-    cookies=cookies,
-    headers=headers,
-)
-print(response.cookies)
+    headers = {
+        'Host': 'bjrcb.mocentre.cn',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 13; M2012K11C Build/TKQ1.220829.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/104.0.5112.97 Mobile Safari/537.36 App/BJRCB',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'X-Requested-With': 'cn.com.bjns.mbank',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-User': '?1',
+        'Sec-Fetch-Dest': 'document',
+        'Referer': 'https://bjrcb.mocentre.cn/Wap/PolyPage/index?keymark=HdzqName&login_flag=1&Cid=04B4ED68F430C5C0EB0FC747913BA21B06B395FA476B3B211F1737507EC1C65DF6BCBCF7F5569CA6B17EDF9312D5DE8CC6AC02EBC970958D6802BA81D6BAA1D741168D3DDAA089CF6B9075693692AE1392FCC685B361F1AC8371DDED2DF54BB342025A1B095073292789943E',
+        # 'Accept-Encoding': 'gzip, deflate',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+        # 'Cookie': 'think_language=zh-CN; PHPSESSID=ak7q655p8c24ni7auceik34h7b; bjrcb_info=04B4ED68F430C5C0EB0FC747913BA21B06B395FA476B3B211F1737507EC1C65DF6BCBCF7F5569CA6B17EDF9312D5DE8CC6AC02EBC970958D6802BA81D6BAA1D741168D3DDAA089CF6B9075693692AE1392FCC685B361F1AC8371DDED2DF54BB342025A1B095073292789943E',
+    }
+
+    response = requests.get(
+        'https://bjrcb.mocentre.cn/Wap/LotteryDrawCommon/index/alone/1/token/mocentreabc/lotteryKey/fenghuang1.html',
+        cookies=cookies,
+        headers=headers,
+    )
+    data = str(response.cookies)
+    pattern = r'fenghuang1_user=(.*?) for'
+
+    match = re.search(pattern, data)
+    if match:
+        variable_value = match.group(1)
+        variable_value=str(variable_value)
+        print(variable_value)
+        return variable_value
+    else:
+        print("Variable not found.")
+
+fb()
