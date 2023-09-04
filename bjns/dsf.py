950c6822ac91d725fa6eaaa56535f6cffdcb222c
35626 <ztyawc@gmail.com>
2023-09-04 17:14:51 +0800

    完全体

--- bjns/dsf.py
+++ bjns/dsf.py
@@ -1,56 +1,65 @@
-import tkinter as tk
-from tkinter import ttk
-from tkinter import messagebox
-from datetime import datetime
-import random
+import datetime
 
-def print_entry():
-    text.insert(tk.END, entry.get() + '\n')
-
-def calculate_birth_year():
-    current_year = datetime.now().year
-    birth_year = current_year - int(age_entry.get())
-    text.insert(tk.END, '你是' + str(birth_year) + '年出生的\n')
-    if age_entry.get() == '99':
-        messagebox.showinfo("祝福", "祝您活到99岁")
-        root.destroy()
-
-def on_closing():
-    if messagebox.askokcancel("退出", "你确定要退出吗?"):
-        root.geometry(f'{random.randint(100, 1000)}x{random.randint(100, 1000)}')
-
-root = tk.Tk()
-root.title("图形化界面")
-root.geometry('500x500')  # 调整窗口大小
-root.protocol("WM_DELETE_WINDOW", on_closing)
-
-frame = tk.Frame(root)
-frame.pack(padx=10, pady=10)  # 增加内边距
-
-left_frame = tk.Frame(frame)
-left_frame.pack(side=tk.LEFT, padx=10, pady=10)  # 增加内边距
-
-right_frame = tk.Frame(frame)
-right_frame.pack(side=tk.RIGHT, padx=10, pady=10)  # 增加内边距
-
-entry = ttk.Entry(left_frame, width=30)  # 调整输入框大小
-entry.pack(pady=10)  # 增加内边距
-
-button = ttk.Button(left_frame, text="打印输入", command=print_entry)
-button.pack(pady=10)  # 增加内边距
-
-age_entry = ttk.Entry(left_frame, width=30)  # 调整输入框大小
-age_entry.pack(pady=10)  # 增加内边距
-
-age_button = ttk.Button(left_frame, text="计算出生年份", command=calculate_birth_year)
-age_button.pack(pady=10)  # 增加内边距
-
-text = tk.Text(right_frame, width=30, height=10)  # 调整文本框大小
-text.pack(pady=10)  # 增加内边距
-
-root.mainloop()
+import requests
 
 
 
 
+def week_task():
+    today = datetime.datetime.now().weekday()
+    if today == 0:  # Monday
+        URL = 'https://www.south-plus.net/plugin.php?H_name=tasks&action=ajax&actions=job&cid=14&nowtime=1693814727224&verify=f0ad8f41'
+        HEADERS = {
+            'authority': 'www.south-plus.net',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'zh-CN,zh;q=0.9',
+            'cookie': 'eb9e6_cknum=AwZcVw0CAFUNXDtqVlsJAgABCwRTVVMHUVRbAlYAUghcVgdQAQVcVQILV1U%3D; eb9e6_ck_info=%2F%09; eb9e6_winduser=AwdXUQMHAm0MAgVbAAoJVFdRVgQBUwFWAAILUVBXUA4PUAdUUVJXXDw%3D; eb9e6_ol_offset=80025; peacemaker=1; eb9e6_lastpos=other; eb9e6_lastvisit=87%091693814246%09%2Fplugin.php%3FH_name%3Dtasks%26action%3Dajax%26actions%3Djob%26cid%3D15%26nowtime%3D1693814245244%26verify%3Df0ad8f41',
+            'referer': 'https://www.south-plus.net/plugin.php?H_name-tasks.html',
+            'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'iframe',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
+        }
 
+        def get_response():
+            response = requests.get(URL, headers=HEADERS)
+            return response.text
+
+        print(get_response())
+
+week_task()
+
+
+
+
+def day_task():
+    URL = 'https://www.south-plus.net/plugin.php?H_name=tasks&action=ajax&actions=job&cid=15&nowtime=1693814245244&verify=f0ad8f41'
+    HEADERS = {
+        'authority': 'www.south-plus.net',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept-language': 'zh-CN,zh;q=0.9',
+        'cookie': 'eb9e6_cknum=AwZcVw0CAFUNXDtqVlsJAgABCwRTVVMHUVRbAlYAUghcVgdQAQVcVQILV1U%3D; eb9e6_ck_info=%2F%09; eb9e6_winduser=AwdXUQMHAm0MAgVbAAoJVFdRVgQBUwFWAAILUVBXUA4PUAdUUVJXXDw%3D; eb9e6_ol_offset=80025; peacemaker=1; eb9e6_lastpos=other; eb9e6_lastvisit=69%091693814228%09%2Fplugin.php%3FH_name-tasks.html',
+        'referer': 'https://www.south-plus.net/plugin.php?H_name-tasks.html',
+        'sec-ch-ua': '"Chromium";v="116", "Not)A;Brand";v="24", "Google Chrome";v="116"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'sec-fetch-dest': 'iframe',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-user': '?1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
+    }
+
+    def get_response():
+        response = requests.get(URL, headers=HEADERS)
+        return response.text
+
+    print(get_response())
+
+day_task()
