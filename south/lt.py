7bb518553d8b058d24ba878e4d1866a7695a1f16
Administrator <ztyawc@gmail.com>
2024-08-16 01:15:54 +0800

    1

--- south/lt.py
+++ south/lt.py
@@ -3,29 +3,24 @@ import requests
 import ding
 
 url = "https://m.client.10010.com/servicequerybusiness/operationservice/queryOcsPackageFlowLeftContentRevisedInJune"
-cookie = ("ecs_acc=kIWBYZZjQKyN5QCbldViwRJlLFgwKx27WeUypXj3U+GzAVlde+KLKxD24X+HWuHEcTI2PedEh2CMrF0pkjzPO0rkW7uAh+yG5S8yAaIQQtuMKvFkXFvPwIe/NYKrPSfLUA9UVNKz4i98SJcluyBYGUqkBELTXFBiL1pqiCXrjvM=; "
-          "ecs_token=eyJkYXRhIjoiYWVjZTQ5NTI5ZWVkODY0ZmNlMTA3NzdmODYxZDk3YTc2ZGYyOWRmOWVjY2I0ZWU4MGUwN2IxNzA4NDkxOTFkODEzNzNhMmE5ZWVkNWU5Yjg2NzBhYjcyZGNkZDRiYWVkZDc5ODFkZjViYWRjNTNlOWE5ZDE4N2ZmOWEwNGUxZTA2MmEzMDIwYWY1ZDQ0Mzc0M2QzODViZDIwZDc0ZGQ5M2M3ODQ5ZmJlMWRkZjVhMDBlOWE1Yzg3YjlmN2Q1MTI1NDZhNzM0ZWQ1ZTkwNTM1OTE4OTcwYWJhMmE2ZjA4OTNhODI3NGFmNmNiMzUwZTg5Mjc2MTU2ZjllYjI3ZDNhYmZhZTE2MzExNzUyMmJmMjkzYzJlNjQ1YmE2MzA5MjI0IiwidmVyc2lvbiI6IjAwIn0=")
+
 headers = {
     "Host": "m.client.10010.com",
-    "content-length": "137",
-    "pragma": "no-cache",
-    "cache-control": "no-cache",
-    "sec-ch-ua": '"Not/A)Brand";v="8", "Chromium";v="126", "Android WebView";v="126"',
-    "accept": "application/json, text/plain, */*",
-    "content-type": "application/x-www-form-urlencoded",
-    "sec-ch-ua-mobile": "?1",
-    "user-agent": "Mozilla/5.0 (Linux; Android 14; 23127PN0CC Build/UKQ1.230804.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/126.0.6478.71 Mobile Safari/537.36; unicom{version:android@11.0702,desmobile:0};devicetype{deviceBrand:Real Manufacturer,deviceModel:Real Device}",
-    "sec-ch-ua-platform": '"Android"',
-    "origin": "https://img.client.10010.com",
-    "x-requested-with": "com.sinovatech.unicom.ui",
-    "sec-fetch-site": "same-site",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-dest": "empty",
-    "referer": "https://img.client.10010.com/",
-    "accept-encoding": "gzip, deflate, br, zstd",
-    "accept-language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
-    "cookie": cookie,
-    "priority": "u=1, i"
+    "Connection": "keep-alive",
+    "sec-ch-ua": '"Not)A;Brand";v="99", "Google Chrome";v="127", "Chromium";v="127"',
+    "Accept": "application/json, text/plain, */*",
+    "sec-ch-ua-platform": '"Windows"',
+    "sec-ch-ua-mobile": "?0",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Origin": "https://img.client.10010.com",
+    "Sec-Fetch-Site": "same-site",
+    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Dest": "empty",
+    "Referer": "https://img.client.10010.com/",
+    "Accept-Encoding": "gzip, deflate, br, zstd",
+    "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
+    "Cookie": "SHAREJSESSIONID=312F9070E818FB87AE1FA08D00B3A4E0; ecs_acc=; req_mobile=; req_serial=; clientid=98|0; req_wheel=ssss; mallcity=19|190; userprocode=019; selectedProvince=019; arialoadData=false; acw_tc=1a0c655417237402606605926e003d8ba6fa3350a3af999b94acec09a96658; ecs_cook=7a96c526c8beccdfaa50adcacd8357cb; unicomMallUid=2DVHfE11pXokoqB; JUT=nVVjwYRhiKXndK4OhyXhkeyEx2YwhY1uVa2VqKD0Qneh9/spXb/CfYnIlrh8NhBXSjbx0gUqFdhGIGJhievzSPLdo8fCkHxkT37Ytvy/wIUPxT5l8UOEq4qG6zoMex5/UzUE/K0M7NkcIkpl6XEP8V+97LpDQyPRuWipTtG2Z8yoMNmBzwWWRg1rcs7HSYLPVVDDaCkMPIMu6RM+lzXPnFABF80YO52KOd9HN+gqtbVAha1iOziYlpn4qDgQEHmnQjs262zzFZFZKqATp6dq/KyXWpYg8D3I07W8j+WSBGwoEi5FMzg/usHbiFmrFjhDafaPhVy4CiPy+xnwO/oFlGMRwHQ1kolKk71GMgt6qIhbjo94c0NJ+IOp7F+DHR4QhrJ3c8SjmkzbqwFnDTALXZDWyi7So/f0LFzb7wgP5Olq2ZLPoQ++x7ZAURy+pppS3K11RQ3IYXXdNM1MNE712OvTWUPly3TtAQs8kzpfx3q2ExaYXHBfSvKZW/Fhstdw2ubzsVTeqXPxLxrYZVQ/00c+CbO7xq/qMllUV+Mt1yMul/I3LsyZaSQ3XYFicW3LpTcQPMsQB6rUuuyQwIUYdnZKC4mn4uUZGX4CrZCmoSJErqGPWZ1JuYPVGuGVzLPVxuVH4UNLfdjA82vUTsez5Q==; piw=%%7B%%22login_name%%22%%3A%%22155****0817%%22%%2C%%22nickName%%22%%3A%%22155****0817%%22%%2C%%22rme%%22%%3A%%7B%%22ac%%22%%3A%%22%%22%%2C%%22at%%22%%3A%%22%%22%%2C%%22pt%%22%%3A%%2201%%22%%2C%%22u%%22%%3A%%2215536220817%%22%%7D%%2C%%22verifyState%%22%%3A%%22%%22%%7D; loginflag=true; citycode=193; u_account=xfRrzV%%252B%%252BZ17%%252FazcmXDyCvg%%253D%%253D; WT=155****0817; city=019|"
 }
 
 data = {
@@ -38,15 +33,21 @@ data = {
     "ticket": "",
     "ticketPhone": "",
     "ticketChannel": "",
+    "version": "WT",
+    "userNumber": "",
     "language": "chinese"
 }
-
 response = requests.post(url, headers=headers, data=data)
 
-# 获取日租宝和其他流量使用量
-day_addUpItemName = response.json()['RzbResources'][0]['details'][0]['feePolicyName']
-day_remain = response.json()['RzbResources'][0]['details'][0]['remain']
-day_use = float(response.json()['RzbResources'][0]['details'][0]['use'])
+if (float(response.json()['RzbResources'][0]['rzbAllUse'])==0.0):
+    day_addUpItemName="日租宝"
+    day_remain = "0"
+    day_use = float("0")
+else:
+    # 获取日租宝和其他流量使用量
+    day_addUpItemName = response.json()['RzbResources'][0]['details'][0]['feePolicyName']
+    day_remain = response.json()['RzbResources'][0]['details'][0]['remain']
+    day_use = float(response.json()['RzbResources'][0]['details'][0]['use'])
 r_use = float(response.json()['resources'][0]['details'][0]['use'])
 # 获取其他资源的使用量和剩余量
 m_use_gb = format(int(float(response.json()['MlResources'][0]['details'][1]['use'])) / 1024, ".2f")
@@ -88,7 +89,7 @@ with open(file_path_r, "w") as file:
 
 
 print("免流用量："+m_use_gb+"\n"+
-      "日租宝已使用："+str(day_use)+"日租宝剩余："+(day_remain)+"\n"
-      "免费1GB已使用："+str(r_use)+"免费1GB剩余："+(r_remain)
+      "日租宝已使用："+str(day_use)+   "   日租宝剩余："+(day_remain)+"\n"
+      "免费1GB已使用："+str(r_use)+   "   免费1GB剩余："+(r_remain)
       )
 
